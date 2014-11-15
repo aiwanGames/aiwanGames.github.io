0be1c834e115f3447e19788c2fc342173ab925fe
aiwanGames <49149092@qq.com>
2014-11-15 19:54:42 +0800

    ver2.0

---
+++ suishoujiGame/src/resource.js
@@ -0,0 +1,36 @@
+var s_back="res/img_back.png";
+var s_kaishi="res/img_btn_kaishi.png";
+var s_zailaiyici="res/img_btn_zailaiyici.png";
+var s_catch="res/img_catch.png";
+var s_jiayou="res/img_jiayou.png";
+var s_pojilu="res/img_pojilu.png";
+var s_point="res/img_point_01.png";
+var s_result="res/img_result.png";
+var s_icon="res/img_icon.png";
+var s_icon_bg="res/img_icon_bg.png";
+var s_tips1="res/img_tips1.png";
+var s_tips2="res/img_tips2.png";
+
+var g_resources = [
+    //image
+    {src:s_back},
+    {src:s_kaishi},
+    {src:s_zailaiyici},
+    {src:s_catch},
+    {src:s_jiayou},
+    {src:s_pojilu},
+    {src:s_point},
+    {src:s_result},
+    {src:s_icon_bg},
+    {src:s_tips1},
+    {src:s_tips2},
+    //plist
+    {src:s_icon}
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
