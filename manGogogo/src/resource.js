e6bc08c4964a73412419dfe292f205c2c0afa2a7
aiwanGames <49149092@qq.com>
2014-11-18 20:46:33 +0800

    ver2.1

---
+++ manGogogo/src/resource.js
@@ -0,0 +1,46 @@
+var s_go="img_go.png";
+var s_go2="img_go_press.png";
+var s_man="img_man.png";
+var s_manplist="img_man.plist";
+var s_block1="img_block01.png";
+var s_block2="img_block02.png";
+var s_time="img_time.png";
+var s_level="img_level.png";
+var s_number="img_number1.png";
+var s_left="img_left.png";
+var s_left_press="img_left_press.png";
+var s_right="img_right.png";
+var s_right_press="img_right_press.png";
+var s_again_press="img_again_press.png";
+var s_again="img_again.png";
+var s_share="img_share.png";
+var s_fenxiang="img_fenxiang.png";
+
+var g_resources = [
+    //image
+    {src:s_go},
+    {src:s_go2},
+    {src:s_man},
+    {src:s_block1},
+    {src:s_block2},
+    {src:s_time},
+    {src:s_level},
+    {src:s_number},
+    {src:s_left},
+    {src:s_left_press},
+    {src:s_right},
+    {src:s_right_press},
+    {src:s_again},
+    {src:s_again_press},
+    {src:s_share},
+    {src:s_fenxiang},
+    //plist
+    {src:s_manplist}
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
