89764636b6760d84372144f83e3eeea2551040ca
aiwanGames <49149092@qq.com>
2014-11-23 22:19:12 +0800

    ver3.0

--- manGogogo/main.js
+++ manGogogo/main.js
@@ -49,7 +49,7 @@ var cocos2dApp = cc.Application.extend({
         //cc.EGLView.getInstance()._adjustSizeToBrowser();//此处不需要，不然用微信打开后，屏幕会有偏移
         cc.EGLView.getInstance().adjustViewPort(true);
         cc.EGLView.getInstance().setDesignResolutionSize(640, 960, cc.RESOLUTION_POLICY.SHOW_ALL);
-        cc.EGLView.getInstance()._resizeWithBrowserSize(true);
+        cc.EGLView.getInstance().resizeWithBrowserSize(true);
 
         // turn on display FPS
         //director.setDisplayStats(this.config['showFPS']);
