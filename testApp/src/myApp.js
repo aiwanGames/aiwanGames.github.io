32e4824c1f56f557c3680ea0d66046bd43be4ade
aiwanGames <49149092@qq.com>
2014-12-11 19:57:12 +0800

    ver1.8

--- testApp/src/myApp.js
+++ testApp/src/myApp.js
@@ -39,20 +39,30 @@ var beginLayer = cc.LayerColor.extend({
         label.setPosition(cc.p(this.winsize.width*0.5,this.winsize.height*0.5));
         this.addChild(label,1);
 
+        var menuItem=cc.MenuItemImage.create(s_img02,s_img03,this.gotoURL,this);
+        var menu=cc.Menu.create(menuItem);
+        menu.setPosition(cc.p(this.winsize.width*0.5,this.winsize.height*0.2));
+        this.addChild(menu,1);
+
+        //在URL中加入ID可根据URL判断显示内容哟，如index.html?age=10size=5！！！
         if(document.URL.indexOf("id=2")>=0)
         {
             this.setColor(cc.c4(0,0,0,255));
         }
-        if(document.URL.indexOf("id=2")>=0)
-        {
-            this.setColor(cc.c4(0,0,0,255));
-        }
-        //document.title=window.wxData.desc = "2nd";
-        //document.title=window.wxFriend.desc = "2nd";
         window.wxData.link=window.wxData.link+"index.html?id=2";
         window.wxFriend.link=window.wxFriend.link+"index.html?id=2";
-        //cc.log(window.wxFriend.link);
         return true;
+    },
+
+    gotoURL:function()
+    {
+        //在index.html中定时跳转
+        //<meta http-equiv=refresh content=10;URL="http://www.baidu.com">
+
+        //用按钮实现跳转，URL必须有http前缀，比如只填写www.baidu.com是跳转不了的
+        var newURL="http://www.baidu.com";
+        window.location.href=newURL;
+        //window.open("www.baidu.com","_self");
     }
 });
 
