2be502823f905bcf16acd933e66e708de0621345
aiwanGames <49149092@qq.com>
2015-01-29 11:34:37 +0800

    ver2.2

--- ssGameFour/cocos2d.js
+++ ssGameFour/cocos2d.js
@@ -74,7 +74,7 @@
         }
         /*********Delete this section if you have packed all files into one*******/
 
-        //s.src = 'ssGameFour.js'; //IMPORTANT: Un-comment this line if you have packed all files into one
+        s.src = 'ssGameFour.js'; //IMPORTANT: Un-comment this line if you have packed all files into one
 
         d.body.appendChild(s);
         document.ccConfig = c;
