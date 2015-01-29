16c0ccb578ae7e319dc4000d7c3fcee0ea950fc7
aiwanGames <49149092@qq.com>
2015-01-29 13:59:55 +0800

    ver3.3

--- ssGameFour/src/myApp.js
+++ ssGameFour/src/myApp.js
@@ -114,10 +114,9 @@ var beginLayer = cc.LayerColor.extend({
             this.textField.detachWithIME();
             var cnt=this.textField.getContentText();
             this.textField.setString("33333");
-            this.textField.insertText("2",1);
             this.textField.setColorSpaceHolder(cc.c3(0,0,0));
             this.textField.setPosition(cc.p(320,780));
-            this.textField.draw();
+            //this.textField.draw();
         }
     },
 
